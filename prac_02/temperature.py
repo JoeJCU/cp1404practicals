b9fec0f0e752412f86d883e9199a1e56050156c5
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-04 00:55:09 +1000

    temp update

--- prac_02/temperature.py
+++ prac_02/temperature.py
@@ -1,42 +1,36 @@
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("fahrenheit: "))
-        celsius = (fahrenheit - 32) / 1.8
-        print(f"Result: {celsius:.2f} F")  # too two decimal places(:.2f)
 
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        pass
-    else:
-        print("Invalid option")
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsiusfahrenheit(celsius)
+            print(f"{celsius} Celsius converts to {fahrenheit:.2f} Fahrenheit")
+
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_celsius(fahrenheit)
+            print(f"{fahrenheit} Fahrenheit converts to {celsius:.2f} Celsius")
+
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def celsiusfahrenheit(celsius):
+    fahrenheit = (celsius * 9) / 5 + 32 #celsius to fah
+    return fahrenheit
+
+def fahrenheit_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32) #fah to celsius
+    return celsius
 
 
-
-
-def fahrenhit_celcius(fahrenheit):
-    return (fahrenheit - 32) / 5/9 # conversion to convert fahrenhit to celcius
-
-celsius = fahrenhit_celcius(fahrenheit)
-print(f"{fahrenheit} Fahrenheit converts to {celsius:.2f} Celsius")
-
-
-
-
-def celcius_fahrenheit(celsius):
-    return (celsius * 9.0) / 5 + 32
-
-fahrenheit = celcius_fahrenheit(celsius)
-print(f"{celsius} Celsius converts to {fahrenheit:.2f} Fahrenheit")
+main()
