4e6bcf7a93b216c7e0a2dd9ab02bcb081cc9d5d9
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-04 00:34:31 +1000

    temp update

--- prac_02/temperature.py
+++ prac_02/temperature.py
@@ -1,8 +1,30 @@
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print(f"Result: {fahrenheit:.2f} F")
+    elif choice == "F":
+        fahrenheit = float(input("fahrenheit: "))
+        celsius = (fahrenheit - 32) / 1.8
+        print(f"Result: {celsius:.2f} F")  # too two decimal places(:.2f)
 
-fahrenheit = float(input("Enter the temperature in Fahrenhit: "))
+        # TODO: Write this section to convert F to C and display the result
+        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+        # Remove the "pass" statement when you are done. It's a placeholder.
+        pass
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
+
+
+
 
 def fahrenhit_celcius(fahrenheit):
     return (fahrenheit - 32) / 5/9 # conversion to convert fahrenhit to celcius
@@ -12,9 +34,6 @@ print(f"{fahrenheit} Fahrenheit converts to {celsius:.2f} Celsius")
 
 
 
-
-
-celsius = float(input("Enter the temperature in Celsius: "))
 
 def celcius_fahrenheit(celsius):
     return (celsius * 9.0) / 5 + 32
