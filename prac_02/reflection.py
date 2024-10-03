7885de4b397bb7ce8a0aca187381084e70f27792
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-04 00:33:16 +1000

    Reflection

---
+++ prac_02/reflection.py
@@ -0,0 +1,19 @@
+"""What percentage of the lecture videos have you watched so far?
+I watched one or two of week one and two. Quickly skimmed through the rest.
+
+What time, location and duration have you scheduled to watch lecture videos?
+Mainly at home, during the night
+
+What are you doing well in the subject so far that you will keep doing?
+Attemping the tasks.
+
+What do you need to stop or change in your work for this subject overall so far?
+Need to going over all the content.
+
+What are you currently doing poorly for practicals in this subject that you will change?
+Time management
+
+What are you doing really well for practicals in this subject?
+If statements and testing my code to see if it works or not. Also, commenting my work so I know what each line/function
+means.
+"""
