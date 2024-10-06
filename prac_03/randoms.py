c881924de386a24621d3b4134f92f911d04e2064
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-07 04:14:50 +1100

    update

---
+++ prac_03/randoms.py
@@ -0,0 +1,24 @@
+import random
+
+
+  # line 1
+print(random.randint(5, 20))
+# 8, 15, 13, 17
+#What was the smallest number you could have seen, what was the largest?
+# 5 & 20
+
+
+print(random.randrange(3, 10, 2))  # line 2
+#3,3,5,5,3
+#What was the smallest number you could have seen, what was the largest?
+#3 and 5
+#Could line 2 have produced a 4?
+#no
+
+print(random.uniform(2.5, 5.5))  # line 3
+#4.29, 5.28, 4.6, 4.8
+#What was the smallest number you could have seen, what was the largest?
+# 2.5 and 5.49
+
+
+print(random.randint(1, 100))
