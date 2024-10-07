4397c5b07782ccd5a6c2bcbe61ee906acea1d401
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-08 00:25:58 +1100

    update

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,8 +3,22 @@
 
 user_name = input("Enter your name: ")
 new_file = "name.txt" #creating the file
-new_file_open = open(new_file, "w") #write
-print(user_name, file = new_file_open)
+new_file_open = open(new_file, "w") #opens name.txt to write
+print(user_name, file = new_file_open, end='') #writes the user name input to the file
 
-new_file_open.close()
-s
+new_file_open.close() #closing
+
+
+
+#2
+
+new_file_read = open(new_file, "r")
+print(f"Hi {new_file_read.read()}!")
+
+
+new_file_read.close()
+
+
+#3
+
+numbers_file = "numbers.txt"
