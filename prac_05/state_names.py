2f57f598811ec4105b7bf888b0cbf7cdafe51460
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-19 16:13:44 +1000

    prac 5. state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -15,11 +15,18 @@ CODE_TO_NAME = {"QLD": "Queensland",
                 "SA": "South Australia"}
 
 print(CODE_TO_NAME)
+for state, name in CODE_TO_NAME.items(): #the .items command returns the whole dict. Created 'state' and 'name' to specifically call the individual states and full names
+    print(f"{state} is {name}")
 
 state_code = input("Enter short state: ").upper()
 print(state_code)
 
+
 while state_code != "":
+
+
+
+
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
