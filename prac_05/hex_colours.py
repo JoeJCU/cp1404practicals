ac7eb116a4863796cc260db7658d464231d9a903
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-19 15:38:45 +1000

    prac 5

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,27 +1,27 @@
-
-
-#creating a dictionery
+# creating a dictionery
 
 hex_colours = {
-    "Cardinal" : "#c41e3a",
-    "Crystal" : "#a7d8de",
-    "MediumOrchid" : "#ba55d3",
-    "Middle Green" : "#4d8c57",
+    "Cardinal": "#c41e3a",
+    "Crystal": "#a7d8de",
+    "MediumOrchid": "#ba55d3",
+    "Middle Green": "#4d8c57",
     "beige": "#f5f5dc",
     "black": "#000000",
-
 
 }
 
 
 def main():
-    user_colour = input("Enter a colour name: ")
+    while True:
+        user_colour = input("Enter a colour name: ")
 
-    if hex_colours.get(user_colour):
-        print(f"The hex code is {hex_colours} for {user_colour}")
-    else:
-        print(f"Please re-enter another colour name!")
+        if hex_colours.get(user_colour):
+            print(f"The hex code is {hex_colours.get(user_colour)} for {user_colour}")
+        else:
+            print(f"Please re-enter another colour name!")
 
+        if user_colour == "":
+            print("Bye")
 
 
 main()
