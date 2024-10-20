5b4677ba92d644c43605aea99d1f1668fae2fc38
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-10-20 22:14:20 +1000

    prac 5. emails

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,7 +1,7 @@
 
 email_dicts = {
 
-}
+} #dict
 
 def main():
     while True:
@@ -10,11 +10,24 @@ def main():
         if user_email == "":
             continue # restarts the loop so that the user have to re enter/enter their email=
 
-        user_name = user_email.split('@')
-        print(user_name)
+        user_name = user_email.split('@')[0] #splits the input at '@'
+        # print(user_name)
 
-        user_confirm = input(f"Is your name {user_name}?(Y/n)")
-        if user_confirm == "Y":
+        user_confirm = input(f"Is your name {user_name}?(y/n)")
+        if user_confirm != "y":
+        #     print(user_name)
+        # else:
+            user_name = input("Enter your name: ")
+
+        #adding the name to the email_dicts dict
+        for name in user_name:
+            email_dicts[name] = user_name
+
+
+        # displays all names and the email
+        for name, email in email_dicts.items():
+            print(f"name:{name}, email: {email})")
+
 
 
 
