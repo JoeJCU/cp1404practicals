3cc746c4c8ad3b69c3648bd4bcc6228ad6856f25
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-11-10 18:30:46 +1000

    prac 7. guitar

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,23 +1,27 @@
 
 import csv
 
+from setuptools.config.expand import read_files
+
 from guitar import guitar_class
 
 def main():
     filename = 'guitars.csv'
     guitars = []
 
-    in_file = open(filename, 'r')
-    in_file.readline()
+    read_file = open(filename, 'r')
+    read_file.readline()
 
-    for data in in_line:
-        guitar = guitar_class(row[0], row[1], row[2])
+    for data in read_file:
+        guitar = guitar_class(data[0], int(data[1]), float(data[2]))   #need to change the format of the data passing in, into int and float
         guitars.append(guitar)
+    read_file.close()
 
+    print("Unsorted Guitars")
+    for guitar in  guitars:
+        print(guitar)
 
-
-
-
-
+    guitars.sort()  # sorting
+    print("\n Sorted Guitars: ")
 
 main()
