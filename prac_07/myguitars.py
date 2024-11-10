53071e86ef5422ccf24fbccf27dea83e4b07fddd
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-11-10 17:59:14 +1000

    prac 7. guitar

---
+++ prac_07/myguitars.py
@@ -0,0 +1,23 @@
+
+import csv
+
+from guitar import guitar_class
+
+def main():
+    filename = 'guitars.csv'
+    guitars = []
+
+    in_file = open(filename, 'r')
+    in_file.readline()
+
+    for data in in_line:
+        guitar = guitar_class(row[0], row[1], row[2])
+        guitars.append(guitar)
+
+
+
+
+
+
+
+main()
