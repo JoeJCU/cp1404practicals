94de03fe6e860a0dea1ccecb160046d7b3927ad7
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-11-23 15:33:01 +1000

    prac 9, taxi class variable

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,7 +3,7 @@
 
 from taxi import Taxi
 
-new_taxi = Taxi(name = "Prius 1", fuel=100, price_per_km=1.23) #new object
+new_taxi = Taxi(name = "Prius 1", fuel=100) #new object
 
 new_taxi.drive(40)  #drive function in taxi.py
 
@@ -11,5 +11,8 @@ print(new_taxi)
 print(new_taxi.get_fare())
 
 new_taxi.start_fare()
+new_taxi.drive(100)
+
+print(f"Taxi details: {new_taxi}, Fare: {new_taxi.get_fare()}")
 
 
