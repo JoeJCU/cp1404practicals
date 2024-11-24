0087bc250c4ce243f9b0f68d2c6004f3a98d5363
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-11-24 22:49:27 +1000

    prac 9, taxi simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -31,12 +31,26 @@ def main():
             if current_taxi == None:
                 print("You need to choose a taxi before you can drive")
             else:
-
-                print(f"Bill to date: ${bill_to_date}")
+                taxi_drive(current_taxi, bill_to_date)
         else:
             print("Invalid selection")
 
+        print("Options: q)uit, c)choose taxi, d)drive")
         option = input(">>> ")
+
+
+def taxi_drive(current_taxi, bill_to_date):
+    try:
+        distance = float(input("Drive how far? "))
+        current_taxi.start_fare()
+        current_taxi.drive(distance)
+        print(f"Your {current_taxi.name} trip cost you: ${current_taxi.get_fare()}")
+        bill_to_date += current_taxi.get_fare()
+        print(f"Bill to date: ${bill_to_date}")
+
+    except ValueError:
+        print("Not a valid number")
+
 
 def taxi_choose(taxis):
     """Displays the taxis array and allows the user to choose one"""
@@ -54,7 +68,6 @@ def taxi_choose(taxis):
         taxi_choice = int(input("Choose Taxi: "))
 
     return taxi_choice
-
 
 
 
