4246d4831fadb2eb59a4097518e7f5d74046c0eb
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-11-24 15:19:29 +1000

    prac 9, taxi simulator wrote the choosing and input features

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -8,3 +8,6 @@ test_car = Silverservicetaxi("test_car", 100, 4)
 # print(test_car)
 test_car.start_fare()
 test_car.drive(18)
+
+print(f"The fare was {test_car.get_fare()}")
+assert abs(test_car.get_fare() - 48.78) < 0.1, f"Assertion Failed!"
