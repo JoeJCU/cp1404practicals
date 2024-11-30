a4f4b1253793ae59f64f21d6a4363f94b95035e6
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-11-30 23:06:46 +1000

    Adding the exceptions

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -9,7 +9,16 @@ import wikipedia
 user_input = input("Enter a page title or a search phrase:")
 
 while user_input != "":
-    wiki_output = wikipedia.page(user_input)
-    print(f"{wiki_output.title}\n{wiki_output.summary}\n{wiki_output.url}")
+    try:
+        wiki_output = wikipedia.page(user_input)
+        print(f"{wiki_output.title}\n{wiki_output.summary}\n{wiki_output.url}\n")
+    except wikipedia.exceptions.DisambiguationError:
+        print("Disambiguation Error, Please try again!")
+    except wikipedia.exceptions.PageError:
+        print("Page Error, Please try again!")
+    except ValueError:
+        print("Please try again!")
+
+
 
 
