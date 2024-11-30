42385e3f406a27871fb8e63a6d32a995ae0e0f44
JoeJCU <joe.davidmathew@my.jcu.edu.au>
2024-11-30 22:51:04 +1000

    Getting user input and passing it through the wiki search

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -6,4 +6,10 @@ Joe David Mathew
 
 import wikipedia
 
+user_input = input("Enter a page title or a search phrase:")
+
+while user_input != "":
+    wiki_output = wikipedia.page(user_input)
+    print(f"{wiki_output.title}\n{wiki_output.summary}\n{wiki_output.url}")
+
 
